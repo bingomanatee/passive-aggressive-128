b63ca06129a30d2d2aee29567c3cb4268efe8adb
David Edelhart <dave@wonderlandlabs.com>
2013-10-11 16:44:22 -0700

    Adding eventful model

--- frames/events/hives/main/actions/search/search_action.js
+++ frames/events/hives/main/actions/search/search_action.js
@@ -2,72 +2,6 @@ var _ = require('underscore');
 var util = require('util');
 var path = require('path');
 var fs = require('fs');
-var request = require('request');
-var _DEBUG = false;
-
-/* ------------ CLOSURE --------------- */
-
-var EVENTFUL_SEARCH_URL = 'http://api.eventful.com/json/events/search';
-var ITEMS_PER_PAGE = 50;
-
-function _get_results(context, done) {
-
-    events = null;
-
-    function _add_to_events(err, res, body) {
-        if (err) return done(err);
-
-        try {
-            var new_data = JSON.parse(body);
-        } catch (err) {
-            return done(err);
-        }
-        if (new_data.events && new_data.events.event && new_data.events.event.length) {
-            if (events) {
-                events.events.event = events.events.event.concat(new_data.events.event);
-            } else {
-                events = new_data;
-            }
-        }
-
-        if (new_data.page_number < (new_data.page_count - 1)) {
-            _poll(new_data.page_number + 1);
-        } else {
-            // the exit condition
-
-            context.$out.set('events', events);
-            done();
-        }
-
-    }
-
-    function _poll(page) {
-        var params = {
-            url: EVENTFUL_SEARCH_URL,
-            qs: {
-                app_key: context.$apiary.get_config('eventful_auth_key'),
-                location: context.location, keywords: context.search,
-                page_size: ITEMS_PER_PAGE
-            }
-        };
-
-        if (context.radius) {
-            context.radius = parseInt(context.radius);
-            if (context.radius > 0) {
-                params.qs.search_radius = context.radius;
-            }
-        }
-
-        if (page) {
-            params.qs.page_number = page;
-        }
-
-        console.log('polling events: %s', JSON.stringify(params));
-        request.get(params, _add_to_events);
-    }
-
-    _poll();
-}
 
 /* -------------- EXPORT --------------- */
 
@@ -105,8 +39,7 @@ module.exports = {
     },
 
     on_post_input: function (context, done) {
-
-        _get_results(context, done);
+        this.model('eventful').search(context, done);
     },
 
     on_post_process: function (context, done) {
