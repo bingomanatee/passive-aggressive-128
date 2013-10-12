1a4a4e5c63baf70fc111adf1dd730b337d1f8c26
David Edelhart <dave@wonderlandlabs.com>
2013-10-12 12:34:53 -0700

    limiting time display to today

--- frames/events/hives/main/actions/movies/movies_action.js
+++ frames/events/hives/main/actions/movies/movies_action.js
@@ -2,12 +2,33 @@ var _ = require('underscore');
 var util = require('util');
 var path = require('path');
 var fs = require('fs');
+var moment = require(moment);
 
 /* ------------ CLOSURE --------------- */
 
 LOCATIONS = {
     sanfrancisco: 94103
 };
+
+function _schedule(showtimes, day){
+    var theatres = {};
+
+    showtimes.forEach(function(showtime){
+        if (!theatres[showtime.theatre.id]){
+            theatres[showtime.theatre.id] = {theatre: showtime.theatre, times: []}
+
+        }
+        theatres[showtime.theatre.id].times.push(new moment(showtime.dateTime));
+    });
+
+    if (!day) day = new moment();
+
+    _.each(theatres, function(t){
+        t.times = _.reject(t.times, function(time){
+            return time.diff(day);
+        })
+    })
+}
 
 /* -------------- EXPORT --------------- */
 
@@ -35,6 +56,7 @@ module.exports = {
 
     on_output: function (context, done) {
         context.$out.set('movies', context.movies);
+        context.$out.set('schedule', _schedule);
         done();
     }
 }
