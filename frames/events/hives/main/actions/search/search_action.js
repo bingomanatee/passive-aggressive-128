1db3cfbad1c2b9f7d5d91ea75b7aa09e8ad823a7
David Edelhart <dave@wonderlandlabs.com>
2013-10-11 23:24:26 -0700

    making search optional; allowing for no events returned

--- frames/events/hives/main/actions/search/search_action.js
+++ frames/events/hives/main/actions/search/search_action.js
@@ -47,8 +47,8 @@ module.exports = {
     on_post_input: function (context, done) {
         var model = this.model('eventful');
         model.search(context, function (err, event_data) {
-            if (err){
-               return done(err);
+            if (err) {
+                return done(err);
             }
             console.log('event_data: %s', util.inspect(event_data));
             context.event_data = event_data;
@@ -60,11 +60,17 @@ module.exports = {
     },
 
     on_post_process: function (context, done) {
-        var norm_data = this.model('eventful').normalize(context.event_data.events.event);
-        _.each(norm_data, function (items, name) {
-            // items are a hash, keyed by ID. We only need the values of the hash.
-            context.$out.set(name, _.values(items));
-        });
+        if (parseInt(context.total_items) > 0) {
+
+            var norm_data = this.model('eventful').normalize(context.event_data.events.event);
+            _.each(norm_data, function (items, name) {
+                // items are a hash, keyed by ID. We only need the values of the hash.
+                context.$out.set(name, _.values(items));
+            });
+        } else {
+            context.$out.set('events', []);
+            context.$out.set('venues', []);
+        }
         done();
     },
 
