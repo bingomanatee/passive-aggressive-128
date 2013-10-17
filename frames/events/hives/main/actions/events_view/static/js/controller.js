fb9a8718d31fe2dfcb72d362976e5e924cf3cb56
David Edelhart <dave@wonderlandlabs.com>
2013-10-16 22:31:14 -0700

    shimming labels; putting in references to model system.

--- frames/events/hives/main/actions/events_view/static/js/controller.js
+++ frames/events/hives/main/actions/events_view/static/js/controller.js
@@ -1,14 +1,34 @@
-(function(window){
+(function (window) {
 
-    function paEventsCtrl($scope, Locations, EventTypes, $window){
+    function paEventsCtrl($scope, Locations, EventTypes, $window) {
         $scope.zip = $window._pa_search_query.zip;
         $scope.category = $window._pa_search_query.category;
 
         $scope.locations = Locations.query();
 
+        $scope.$watch('locations', function (locations) {
+            $scope.location = _.find(locations, function (l) {
+                return l.zip == $scope.zip;
+            })
+        }, true);
+
         $scope.location = null;
 
         $scope.event_types = EventTypes.query();
+
+        $scope.$watch('event_types', function (event_types) {
+            $scope.event_type = _.find(event_types, function (et) {
+                return et.id == $scope.category;
+            })
+        }, true);
+
+        $scope.place_label = function(){
+            return $scope.location ? $scope.location.name : $scope.zip;
+        }
+
+        $scope.category_label = function(){
+            return $scope.event_type ? $scope.event_type.name : $scope.category;
+        }
 
     }
 
